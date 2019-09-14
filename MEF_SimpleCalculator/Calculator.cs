8bdd64e3a7e60e6850d74e61b1c6e03c92bd12bd
CloverFeng <sunkesi02@gmail.com>
2019-09-14 12:11:24 +0800

    Create Interface ICalculator And Class Calculator

---
+++ MEF_SimpleCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEF_SimpleCalculator
+{
+    /// <summary>
+    /// 計算機的實作類別
+    /// </summary>
+    public class Calculator : ICalculator
+    {
+        /// <summary>
+        /// 計算機方法
+        /// </summary>
+        /// <param name="inputStr">輸入的計算式</param>
+        /// <returns></returns>
+        public string Calculate(string inputStr)
+        {
+            return $"輸入的計算式:{inputStr}";
+        }
+    }
+}
---
+++ MEF_SimpleCalculator/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEF_SimpleCalculator
+{
+    /// <summary>
+    /// 計算機介面
+    /// </summary>
+    public interface ICalculator
+    {
+        /// <summary>
+        /// 計算機方法
+        /// </summary>
+        /// <param name="inputStr">輸入的計算</param>
+        /// <returns></returns>
+        string Calculate(string inputStr);
+    }
+}
