8bdcaa7e743297839d7df4808100f5b5035bc28f
CloverFeng <sunkesi02@gmail.com>
2019-09-14 11:41:03 +0800

    Create Project Test

--- MEF_SimpleCalculator/Program.cs
+++ MEF_SimpleCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace MEF_SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worl!!");
         }
     }
 }
