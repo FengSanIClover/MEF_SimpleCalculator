d4de4cbecb4e1bc419f74e911b3031a808f409ae
CloverFeng <sunkesi02@gmail.com>
2019-09-14 15:25:35 +0800

    Add Division、Multiplication、Sub Method

---
+++ MEF_SimpleCalculator.Servies/CalculateDivision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace MEF_SimpleCalculator
+{
+    /// <summary>
+    /// 除法邏輯的實作類別
+    /// </summary>
+    [Export(typeof(ICalcLogic))]
+    [ExportMetadata("Symbol", "/")]
+    public class CalculateDivision : ICalcLogic
+    {
+        /// <summary>
+        /// 除法運算
+        /// </summary>
+        /// <param name="left">計算符號左邊的數字</param>
+        /// <param name="right">計算符號右邊的數字</param>
+        public int Calculate(int left, int right)
+        {
+            return left / right;
+        }
+    }
+}
---
+++ MEF_SimpleCalculator.Servies/CalculateSub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace MEF_SimpleCalculator
+{
+    /// <summary>
+    /// 減法邏輯的實作類別
+    /// </summary>
+    [Export(typeof(ICalcLogic))]
+    [ExportMetadata("Symbol", "-")]
+    public class CalculateSub : ICalcLogic
+    {
+        /// <summary>
+        /// 減法運算
+        /// </summary>
+        /// <param name="left">計算符號左邊的數字</param>
+        /// <param name="right">計算符號右邊的數字</param>
+        /// <returns></returns>
+        public int Calculate(int left, int right)
+        {
+            return left - right;
+        }
+    }
+}
